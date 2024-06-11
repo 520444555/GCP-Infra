2495336b7d87e5a4a72a4adf3f154c1a52524efd
Amit Kudtarkar <58769363+520444555@users.noreply.github.com>
2024-06-11 21:24:15 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "roles" {
+  default = ""
+}
+ 
+variable "views" {
+  default = ""
+}
+ 
+variable "datasets" {
+  default = ""
+}
+variable "project" {
+  default = "hsbc-10534429-fdreu-prod"
+}
